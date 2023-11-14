7fdd665a8d4e1e6453ac636699ea32c61b74f8e1
keubung <skdud3390@navercom>
2023-11-14 14:20:45 +0900

    ui 구현(start, setting, exit)

---
+++ Attendance simulation/Assets/UIScript/Exitbtn.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exitbtn : MonoBehaviour
+{
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+}
